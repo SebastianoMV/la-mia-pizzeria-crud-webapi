8d92f45ede2d60063c1b2f254288d73110a1d112
SebastianoMV <sebastianovisco@gmail.com>
2022-10-14 14:10:59 +0200

    Creazione Api PizzaController

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,17 @@
+﻿using la_mia_pizzeria_post.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_post.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            Context _context = new();
+            return Ok();
+        }
+    }
+}
